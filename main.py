abd62eb3adc578523d2c3c795563b1c1db3bcff8
Connnnnn <cjmkeaney@gmail.com>
2020-12-07 23:02:29 +0000

    10 iteration averages and reference algorithm

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import csv
 import random
-from math import log, e
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 
 
 # Used to load in the file data into the dataset
 # Also returns the number of entries in the file
-
 def load_data(file):
     d = []
     att = []
@@ -34,10 +36,11 @@ def split_shuffle(ds, parts):
 
 # Used to returns all values for a set column
 
-def get_column(r, c):
+def get_column(rows, col):
     cols = []
-    for row in r:
-        cols.append(row[c])
+
+    for row in rows:
+        cols.append(row[col])
     return cols
 
 
@@ -66,16 +69,8 @@ def compare(r, test_c, test_val):
     else:
         return False
 
-def check_forValue(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
-
 
 # Splits the data into two lists for the true/false results of the compare test
-
 def fork(r, c, test_val):
     true = []
     false = []
@@ -90,7 +85,7 @@ def fork(r, c, test_val):
     return true, false
 
 
-# Used to calculate the Gini Index of the rows inputted (of beer style)
+# Used to calculate the Gini Index/Impurity of the rows inputted (of beer style)
 
 def gini_index(r):
     stylesNum = y_count(r)
@@ -102,7 +97,7 @@ def gini_index(r):
     return impurity
 
 
-# Used to calculate the Information gain
+# Used to calculate the Information gain, incorporates the gini index (impurity)
 
 def gain(left, right, impurity):
     p = float(len(left)) / (len(left) + len(right))
@@ -116,24 +111,33 @@ def split(r):
     max_ig = 0
     max_att = 0
     max_att_val = 0
-    i = 0
 
+    # calculates gini for the rows provided
     curr_gini = gini_index(r)
-    n_att = len(attribute)
+    no_att = len(r[0])
 
-    for c in range(n_att):
+    # Goes through the different attributes
+
+    for c in range(no_att):
+
+        # Skip the label column (beer style)
+
         if c == 3:
             continue
+        column_vals = get_column(r, c)
 
-        c_vals = get_column(r, c)
+        i = 0
+        while i < len(column_vals):
+            # value we want to check
+            att_val = r[i][c]
 
-        while i < len(c_vals):
+            # Use the attribute value to fork the data to true and false streams
+            true, false = fork(r, c, att_val)
 
-            # Value of the current attribute that is being tested
-            curr_att_val = r[i][c]
-            true, false = fork(r, c, curr_att_val)
+            # Calculate the information gain
             ig = gain(true, false, curr_gini)
 
+            # If this gain is the highest found then mark this as the best choice
             if ig > max_ig:
                 max_ig = ig
                 max_att = c
@@ -144,6 +148,7 @@ def split(r):
 
 
 # Used to recursively go through the tree in order to find the optimal attribute to split the tree with
+
 def rec_tree(r):
     ig, att, curr_att_val = split(r)
 
@@ -158,7 +163,7 @@ def rec_tree(r):
     return Node(att, curr_att_val, true_branch, false_branch)
 
 
-# Defines the classifications of leaf
+# Defines the classifications of the leaf
 
 class Leaf:
     def __init__(self, rows):
@@ -175,31 +180,6 @@ class Node:
         self.false_branch = false_branch
 
 
-def build_tree(node, spacing=""):
-    # Base case: we've reached a leaf
-    if isinstance(node, Leaf):
-        print(spacing + "Predict", node.predictions)
-        return
-
-    print(spacing + "Is " + attribute[node.att] + " > " + str(node.att_value) + " ?")
-
-    # Call this function recursively on the true branch
-    print(spacing + '--> True:')
-    build_tree(node.true_branch, spacing + "  ")
-
-    # Call this function recursively on the false branch
-    print(spacing + '--> False:')
-    build_tree(node.false_branch, spacing + "  ")
-
-
-def print_leaf(counts):
-    total = sum(counts.values())
-    probs = {}
-    for lbl in counts.keys():
-        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
-    return probs
-
-
 # method used to classify test data once tree is formed
 def classify(r, node):
     if isinstance(node, Leaf):
@@ -214,31 +194,90 @@ def classify(r, node):
         return classify(r, node.false_branch)
 
 
-# Main function
+# Prints and formats the tree based on the branches and questions
 
-if __name__ == '__main__':
+def build_tree(node, spacing=""):
+    # If you've reached the terminal state then predict
+    if isinstance(node, Leaf):
+        print(spacing + "Predict", node.predictions)
+        return
+
+    print(spacing + "Is " + attributes[node.att] + " > " + str(node.att_value) + " ?")
+
+    print(spacing + '--> True:')
+    build_tree(node.true_branch, spacing + "  ")
+
+    print(spacing + '--> False:')
+    build_tree(node.false_branch, spacing + "  ")
+
+
+# Prints out the leaf (the beer style)
+
+def print_leaf(counts):
+    total = sum(counts.values())
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+
+if __name__ == "__main__":
+    #
     filename = 'beer.txt'
-    data, classes, attribute = load_data(filename)
+    avg_acc = 0
+    avg_ref_acc = 0
+    i = 0
+    data, classes, attributes = load_data(filename)
 
-    testing, training = split_shuffle(data, 3)
+    # This is for the reference implementation of the decision tree classifier
 
-    # print(testing), print(training), print(data), print(classes)
-    print(attribute)
+    featuredCols = ['calorific_value', 'nitrogen', 'turbidity', 'alcohol', 'sugars', 'bitterness', 'beer_id',
+                    'colour', 'degree_of_fermentation']
+    ref_attributes = ['calorific_value', 'nitrogen', 'turbidity', 'beer_style', 'alcohol', 'sugars', 'bitterness',
+                      'beer_id', 'colour', 'degree_of_fermentation']
 
-    tree = rec_tree(training)
-    build_tree(tree)
+    trainingData = pd.read_csv('training.txt', sep='\t', names=ref_attributes)
+    testData = pd.read_csv('test.txt', sep='\t', names=ref_attributes)
 
-    right = 0
-    wrong = 0
-    for r in testing:
-        # classify(row, tree)
-        print("Actual: %s. Predicted: %s" % (r[3], print_leaf(classify(r, tree))))
-        for key, value in classify(r, tree).items():
-            if r[3] == key:
-                right += 1
-            else:
-                wrong += 1
-    print('Percentage Correctly Classified')
-    print(right / (right + wrong) * 100)
-    print('Percentage Incorrectly Classified')
-    print(wrong / (right + wrong) * 100)
+    while i < 10:
+        testing, training = split_shuffle(data, 3)
+        tree = rec_tree(training)
+        build_tree(tree)
+
+        correct = 0
+        incorrect = 0
+        for r in testing:
+            print("Actual: %s. Predicted: %s" % (r[3], print_leaf(classify(r, tree))))
+            for key, value in classify(r, tree).items():
+                if r[3] == key:
+                    correct += 1
+                else:
+                    incorrect += 1
+        print('Percentage Correctly Classified')
+        print(correct / (correct + incorrect) * 100)
+        print('Percentage Incorrectly Classified')
+        print(incorrect / (correct + incorrect) * 100)
+
+        i += 1
+        avg_acc += correct / (correct + incorrect)
+
+        # REFERENCE IMPLEMENTATION
+
+        x_train = trainingData[featuredCols]
+        y_train = trainingData.beer_style
+        x_test = testData[featuredCols]
+        y_test = testData.beer_style
+
+        dtc = DecisionTreeClassifier()
+        dtc = dtc.fit(x_train, y_train)
+        y_predict = dtc.predict(x_test)
+
+        avg_ref_acc += metrics.accuracy_score(y_test, y_predict)
+
+    print("\nThe Average Accuracy across 10 iterations: ")
+    acc10 = avg_acc / 10 * 100
+    print(acc10)
+
+    refAcc = ((avg_ref_acc / 10) * 100)
+    print("\nThe Average Accuracy for the reference decision tree classifier across 10 iterations: ")
+    print(refAcc)
