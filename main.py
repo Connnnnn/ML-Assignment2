9a47ab9fc96f8321ba7c51a8a008d5e820dae768
Connnnnn <cjmkeaney@gmail.com>
2020-12-07 20:14:17 +0000

    Build tree

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ from math import log, e
 
 def load_data(file):
     d = []
+    att = []
     with open(file, 'r') as f:
         r = csv.reader(f, delimiter='\t')
         i = 0
         for row in r:
-            d.append(row)
-            i += 1
-    return d, i
+            if i == 0:
+                att = row
+                i += 1
+            else:
+                d.append(row)
+                i += 1
+    return d, i, att
 
 
 # Used to shuffle and split the data into 2/3s training data and 1/3 testing data
@@ -52,13 +57,20 @@ def y_count(r):
 # in order to split up the data
 
 def compare(r, test_c, test_val):
-    if r[test_c].is_numeric:
+    if r[test_c].isdigit():
         return r[test_c] == test_val
 
     elif float(r[test_c]) >= float(test_val):
         return True
 
     else:
+        return False
+
+def check_forValue(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
         return False
 
 
@@ -94,8 +106,8 @@ def gini_index(r):
 
 def gain(left, right, impurity):
     p = float(len(left)) / (len(left) + len(right))
-    return impurity - p * gini_index(left) \
-           - (1 - p) * gini_index(right)
+    ig = impurity - p * gini_index(left) - (1 - p) * gini_index(right)
+    return ig
 
 
 # Used to find the best split for data among all attributes
@@ -107,7 +119,7 @@ def split(r):
     i = 0
 
     curr_gini = gini_index(r)
-    n_att = len(attributes)
+    n_att = len(attribute)
 
     for c in range(n_att):
         if c == 3:
@@ -116,18 +128,34 @@ def split(r):
         c_vals = get_column(r, c)
 
         while i < len(c_vals):
+
             # Value of the current attribute that is being tested
             curr_att_val = r[i][c]
             true, false = fork(r, c, curr_att_val)
-            gain = gain(true, false, curr_gini)
+            ig = gain(true, false, curr_gini)
 
-            if gain > max_ig:
-                max_ig = gain
+            if ig > max_ig:
+                max_ig = ig
                 max_att = c
                 max_att_val = r[i][c]
             i += 1
 
     return max_ig, max_att, max_att_val
+
+
+# Used to recursively go through the tree in order to find the optimal attribute to split the tree with
+def rec_tree(r):
+    ig, att, curr_att_val = split(r)
+
+    if ig == 0:
+        return Leaf(r)
+
+    true_rows, false_rows = fork(r, att, curr_att_val)
+
+    true_branch = rec_tree(true_rows)
+    false_branch = rec_tree(false_rows)
+
+    return Node(att, curr_att_val, true_branch, false_branch)
 
 
 # Defines the classifications of leaf
@@ -137,26 +165,80 @@ class Leaf:
         self.predictions = y_count(rows)
 
 
-# class to define a split node - contains the feature and feature value and two child branches
+# Defines a split node - contains the primary attribute its value and the two child branches
 
 class Node:
-    def __init__(self, feature, feature_value, true_branch, false_branch):
-        self.feature = feature
-        self.feature_value = feature_value
+    def __init__(self, att, att_value, true_branch, false_branch):
+        self.att = att
+        self.att_value = att_value
         self.true_branch = true_branch
         self.false_branch = false_branch
+
+
+def build_tree(node, spacing=""):
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print(spacing + "Predict", node.predictions)
+        return
+
+    print(spacing + "Is " + attribute[node.att] + " > " + str(node.att_value) + " ?")
+
+    # Call this function recursively on the true branch
+    print(spacing + '--> True:')
+    build_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print(spacing + '--> False:')
+    build_tree(node.false_branch, spacing + "  ")
+
+
+def print_leaf(counts):
+    total = sum(counts.values())
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+
+# method used to classify test data once tree is formed
+def classify(r, node):
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    c = node.att
+    att_value = node.att_value
+
+    if compare(r, c, att_value):
+        return classify(r, node.true_branch)
+    else:
+        return classify(r, node.false_branch)
 
 
 # Main function
 
 if __name__ == '__main__':
     filename = 'beer.txt'
-    data, classes = load_data(filename)
-    attributes = ['calorific_value', 'nitrogen', 'turbidity', 'beer_style', 'alcohol', 'sugars', 'bitterness',
-                  'beer_id', 'colour', 'degree_of_fermentation']
-
-    print(attributes)
+    data, classes, attribute = load_data(filename)
 
     testing, training = split_shuffle(data, 3)
 
-    print(testing), print(training), print(data), print(classes)
+    # print(testing), print(training), print(data), print(classes)
+    print(attribute)
+
+    tree = rec_tree(training)
+    build_tree(tree)
+
+    right = 0
+    wrong = 0
+    for r in testing:
+        # classify(row, tree)
+        print("Actual: %s. Predicted: %s" % (r[3], print_leaf(classify(r, tree))))
+        for key, value in classify(r, tree).items():
+            if r[3] == key:
+                right += 1
+            else:
+                wrong += 1
+    print('Percentage Correctly Classified')
+    print(right / (right + wrong) * 100)
+    print('Percentage Incorrectly Classified')
+    print(wrong / (right + wrong) * 100)
